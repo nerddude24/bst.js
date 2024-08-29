28bc25f4e0d218e98998dab29eab1695292fd4da
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-08-29 14:34:44 +0100

    Add classes

---
+++ bst.js
@@ -0,0 +1,14 @@
+class TreeNode {
+	constructor(val = null) {
+		this.val = val;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class Tree {
+	constructor(arr, root) {
+		this.arr = arr;
+		this.root = root;
+	}
+}
